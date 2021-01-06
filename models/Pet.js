959c8eb549420c78f37cd216a30095569cf595d5
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-06 19:50:38 +0200

    Pet model

---
+++ models/Pet.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const PetScheme = new mongoose.Schema({
+	type: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	adoptionStatus: {
+		type: String,
+		required: true,
+	},
+	// image: {
+	// 	type: String,
+	// 	required: true,
+	// },
+	height: {
+		type: Number,
+		required: true,
+	},
+	weight: {
+		type: Number,
+		required: true,
+	},
+	color: {
+		type: String,
+		required: true,
+	},
+	bio: {
+		type: String,
+		required: true,
+	},
+	hypoallergenic: {
+		type: Boolean,
+		required: true,
+	},
+	dietaryRestrictions: {
+		type: String,
+		required: true,
+	},
+	breed: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now(),
+	},
+});
+
+module.exports = mongoose.model('Pet', PetScheme);
