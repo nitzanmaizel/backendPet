56cb6a4fc2c11d1e3ca64cfbba649229f5f98ab4
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-07 17:56:54 +0200

    Pets route- add pet

--- routes/pets.js
+++ routes/pets.js
@@ -20,4 +20,68 @@ router.get('/', async (req, res) => {
 	}
 });
 
+// @route    POST /pets
+// @desc     Add pet
+// @access   Privet Admin
+
+router.post(
+	'/addpet',
+	[
+		check('type', 'Type name is required').not().isEmpty(),
+		check('name', 'Name name is required').not().isEmpty(),
+		check('adoptionStatus', 'Adoption Status is required').not().isEmpty(),
+		// check('image', 'Image  is required').not().isEmpty(),
+		check('height', 'Height  is required').not().isEmpty(),
+		check('weight', 'Weight  is required').not().isEmpty(),
+		check('color', 'Color  is required').not().isEmpty(),
+		check('bio', 'Bio  is required').not().isEmpty(),
+		check('hypoallergenic', 'Hypoallergenic  is required').isBoolean(),
+		check('dietaryRestrictions', 'Dietary Restrictions  is required').not().isEmpty(),
+		check('breed', 'Breed  is required').not().isEmpty(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const {
+			type,
+			name,
+			adoptionStatus,
+			// image,
+			height,
+			weight,
+			color,
+			bio,
+			hypoallergenic,
+			dietaryRestrictions,
+			breed,
+		} = req.body;
+
+		try {
+			// Create User fom model ==>
+			let pet = new Pet({
+				type,
+				name,
+				adoptionStatus,
+				// image,
+				height,
+				weight,
+				color,
+				bio,
+				hypoallergenic,
+				dietaryRestrictions,
+				breed,
+			});
+			// Save the Pet ==>
+			await pet.save();
+			res.json('Add success');
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Server Error');
+		}
+	}
+);
+
 module.exports = router;
