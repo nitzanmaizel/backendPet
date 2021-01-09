f6c30d011b20da6a2c4479085e8780a0e093a9f6
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-09 23:34:30 +0200

    check heroku 1.0.0

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.use('/api/users', require('./routes/users'));
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
-
 	app.get('*', (req, res) =>
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 	);
