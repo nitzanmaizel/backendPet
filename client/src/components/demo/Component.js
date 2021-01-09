cd7faefbbdec9bfebdd0cf381b2fbda4a26b85c5
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-10 01:09:02 +0200

    heroku v-1.0.7

--- client/src/components/demo/Component.js
+++ client/src/components/demo/Component.js
@@ -32,3 +32,8 @@
 // };
 
 // export default ContainerForm;
+
+// const CLOUDINARY_NAME = config.get('CLOUDINARY_NAME');
+// const CLOUDINARY_API_KEY = config.get('CLOUDINARY_API_KEY');
+// const CLOUDINARY_API_SECRET = config.get('CLOUDINARY_API_SECRET');
+// const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = process.env;
