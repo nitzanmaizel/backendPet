ee2ec086ef3c8c588a4886705fc12dfa09b6c1ab
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-14 15:13:32 +0200

    Heroku -v 1.0.0

--- client/src/context/auth/authTypes.jsx
+++
@@ -1,7 +0,0 @@
-export const SIGNUP_USER = 'REGISTER_FAIL';
-
-export const LOGIN_USER = 'LOGIN_SUCCESS';
-
-export const LOAD_USER = 'LOAD_USER';
-
-export const LOGOUT = 'LOGOUT';
--- client/src/context/users/userTypes.jsx
+++
@@ -1,5 +0,0 @@
-export const UPDATE_PROFILE = 'UPDATE_PROFILE';
-
-export const GET_USER_BY_ID = 'GET_USER_BY_ID';
-
-export const GET_ALL_USERS = 'GET_ALL_USERS';
