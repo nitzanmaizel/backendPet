4da5a1f1876dae7820c375a4a048669ea8d828c1
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-14 16:51:51 +0200

    heroku -v 1.0.5

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/users', require('./routes/users'));
 // 	);
 // }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server start on http://localhost:${PORT}`);
 });
