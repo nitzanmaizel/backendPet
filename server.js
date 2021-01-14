cee18511767bd6bf18fe1e24be1633daaf3cee1d
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-14 15:30:26 +0200

    heroku -v 1.0.2

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/pets', require('./routes/pets'));
 app.use('/api/users', require('./routes/users'));
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	);
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// 	app.get('*', (req, res) =>
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+// 	);
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
